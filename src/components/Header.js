85517472200fe1e049c6146aa0674bfe3998ec72
Devesh225 <deveshtulshyan@gmail.com>
2022-02-17 19:20:14 +0530

    Header Styling Part-I

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   return (
     <div className="header">
-      <h1>App Header</h1>
+      <div className="header__first"> {/*First Part of Header containing Icon and Search Bar*/}
+        <img src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1645104100~hmac=bbb365c2aca80ced4f9f68e2a672b0c2" alt="" />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="header__second"> {/*Second Part of Header containing Options and Profile*/}
+      </div>
     </div>
   )
 }
