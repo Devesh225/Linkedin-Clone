b95cce27a503c935f32fe827027a24078814fd9e
Devesh225 <deveshtulshyan@gmail.com>
2022-02-26 13:56:17 +0530

    Added CSS rulesets and applied some App changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import HomeSidebar from './components/homeSidebar/HomeSidebar';
 import HomeFeed from './components/homeFeed/HomeFeed';
 import HomeWidgets from './components/homeWidgets/HomeWidgets';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Header />
-      <div className="app__home">
-        <HomeSidebar className='app__homeSidebar' />
-        <HomeFeed className='app__homeFeed'/>
-        <HomeWidgets className='app__homeWidgets'/>
-      </div>
+      { !user ? ( <Login /> ) : (
+        <Fragment>
+          <Header />
+          <div className="app__home">
+            <HomeSidebar className='app__homeSidebar' />
+            <HomeFeed className='app__homeFeed'/>
+            <HomeWidgets className='app__homeWidgets'/>
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 }
